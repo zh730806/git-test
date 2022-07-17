8dcc97abe4b3ee7ce4f7f8be137c601c44de841b
曾捍 <109400508+zh730806@users.noreply.github.com>
2022-07-17 11:42:31 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,14 +8,14 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("HelloGit");
+       System.out.println("HelloGit");
         System.out.println("HelloGitHup");
         System.out.println("HelloGitHup3");
         System.out.println("HelloGitHup4");
         System.out.println("mastertest");
         System.out.println("hot-fix test");
+        System.out.println("push test");
          System.out.println("pull test");
-          System.out.println("pull test"); 
 
     }
 }
