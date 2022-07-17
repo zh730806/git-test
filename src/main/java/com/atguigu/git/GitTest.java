117f0f8430e88173f0fc6838dc0b65c6e1a27cd9
curry <curry@qq.com>
2022-07-17 09:47:23 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloGit");
+        System.out.println("HelloGitHup");
     }
 }
