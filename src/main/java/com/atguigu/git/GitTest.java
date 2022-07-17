04a54116cb199bcb58daab3002d03065403bacf0
curry <curry@qq.com>
2022-07-17 09:45:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Description: GitTest
+ * @Author:
+ * @Date: 2022-07-17
+ * @Version:v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
